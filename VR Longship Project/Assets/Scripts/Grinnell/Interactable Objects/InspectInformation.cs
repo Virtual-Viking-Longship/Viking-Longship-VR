916958a1a42bc2e590a0fe3f8d37f44291709e26
Austin Mason <austin.pj.mason@gmail.com>
2024-12-13 16:24:13 -0600

    Edited settings for Carleton setup

--- VR Longship Project/Assets/Scripts/Grinnell/Interactable Objects/InspectInformation.cs
+++ VR Longship Project/Assets/Scripts/Grinnell/Interactable Objects/InspectInformation.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
+//using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(menuName = "Inspect Information")]
 public class InspectInformation : ScriptableObject
